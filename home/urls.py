f418e5a6da27200f9a7e7bea6e7eb1c924ebb97c
Louise <louise@zanier.org>
2020-04-26 23:31:49 +0200

    Added legal notices

--- home/urls.py
+++ home/urls.py
@@ -3,7 +3,7 @@
 # Filename: urls.py
 # Author: Louise <louise>
 # Created: Sun Apr 26 21:16:25 2020 (+0200)
-# Last-Updated: Sun Apr 26 21:16:39 2020 (+0200)
+# Last-Updated: Sun Apr 26 23:01:47 2020 (+0200)
 #           By: Louise <louise>
 # 
 from django.urls import path
@@ -12,4 +12,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('legal', views.legal, name='legal')
 ]
--- home/views.py
+++ home/views.py
@@ -3,11 +3,19 @@
 # Filename: views.py
 # Author: Louise <louise>
 # Created: Sun Apr 26 21:16:55 2020 (+0200)
-# Last-Updated: Sun Apr 26 21:17:03 2020 (+0200)
+# Last-Updated: Sun Apr 26 23:03:02 2020 (+0200)
 #           By: Louise <louise>
 # 
 from django.shortcuts import render
 
-# Create your views here.
 def index(request):
+    """
+    Home page of the site.
+    """
     return render(request, 'home/index.html')
+
+def legal(request):
+    """
+    Legal notices.
+    """
+    return render(request, 'home/legal.html')
