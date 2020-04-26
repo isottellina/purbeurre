ce8f56058c52256ee3b243667b857f2b18aaa8bf
Louise <louise@zanier.org>
2020-04-27 01:09:46 +0200

    Namespaced urls

--- home/urls.py
+++ home/urls.py
@@ -3,13 +3,14 @@
 # Filename: urls.py
 # Author: Louise <louise>
 # Created: Sun Apr 26 21:16:25 2020 (+0200)
-# Last-Updated: Sun Apr 26 23:01:47 2020 (+0200)
+# Last-Updated: Mon Apr 27 00:15:10 2020 (+0200)
 #           By: Louise <louise>
 # 
 from django.urls import path
 
 from . import views
 
+app_name = 'home'
 urlpatterns = [
     path('', views.index, name='index'),
     path('legal', views.legal, name='legal')
