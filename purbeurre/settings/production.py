d4c0f3621c1f6c8092e9cf8179888d5380fe466d
Louise <louise@zanier.org>
2020-05-07 23:17:10 +0200

    Integrated Sentry

--- purbeurre/settings/production.py
+++ purbeurre/settings/production.py
@@ -3,12 +3,15 @@
 # Filename: production.py
 # Author: Louise <louise>
 # Created: Fri May  1 01:04:57 2020 (+0200)
-# Last-Updated: Thu May  7 17:44:30 2020 (+0200)
+# Last-Updated: Thu May  7 23:07:18 2020 (+0200)
 #           By: Louise <louise>
 #
 import os
 from .base import *
 from django.core.management.utils import get_random_secret_key
+
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 SECRET_KEY = os.environ.get("PURBEURRE_SECRET_KEY", get_random_secret_key())
 DEBUG = False
@@ -47,6 +50,15 @@ LOGGING = {
         },
     },
 }
+
+sentry_sdk.init(
+    dsn=os.environ.get("SENTRY_DSN"),
+    integrations=[DjangoIntegration()],
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
