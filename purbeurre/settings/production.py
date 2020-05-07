efce431ba4acf8e3b7be6ae1a8ec59b19e3abeff
Louise <louise@zanier.org>
2020-05-07 18:39:18 +0200

    Dockerized

--- purbeurre/settings/production.py
+++ purbeurre/settings/production.py
@@ -3,16 +3,17 @@
 # Filename: production.py
 # Author: Louise <louise>
 # Created: Fri May  1 01:04:57 2020 (+0200)
-# Last-Updated: Fri May  1 14:05:12 2020 (+0200)
+# Last-Updated: Thu May  7 17:44:30 2020 (+0200)
 #           By: Louise <louise>
 #
 import os
 from .base import *
+from django.core.management.utils import get_random_secret_key
 
-SECRET_KEY = os.environ.get("PURBEURRE_SECRET_KEY")
+SECRET_KEY = os.environ.get("PURBEURRE_SECRET_KEY", get_random_secret_key())
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "louise-purbeurre.herokuapp.com"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "purbeurre.zanier.org", "*"]
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
@@ -20,23 +21,20 @@ ALLOWED_HOSTS = ["localhost", "127.0.0.1", "louise-purbeurre.herokuapp.com"]
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get("PURBEURRE_DB_NAME"),
-        'USER': os.environ.get("PURBEURRE_DB_USER"),
-        'PASSWORD': os.environ.get("PURBEURRE_DB_PASSWORD"),
-        'HOST': os.environ.get("PURBEURRE_DB_HOST"),
-        'PORT': os.environ.get("PURBEURRE_DB_PORT"),
+        'NAME': os.environ.get("POSTGRES_DB", "postgres"),
+        'USER': os.environ.get("POSTGRES_USER", "postgres"),
+        'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
+        'HOST': os.environ.get("PURBEURRE_DB_HOST", "db"),
+        'PORT': os.environ.get("PURBEURRE_DB_PORT", 5432),
     }
 }
 
-# Middleware, add WhiteNoise to it
-MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATIC_ROOT = '/static'
 
 # Compress settings for production
 # We have to compress offline because WhiteNoise won't
 # service new files.
 COMPRESS_ENABLED = True
-COMPRESS_OFFLINE = True
 
 # We don't need to have legible CSS in production, so
 # we use the compressed output style.
